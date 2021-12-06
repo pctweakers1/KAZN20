cc93c2351cb9ff8c0d254924b662e20115cc6c0d
pctweakers1 <94875011+pctweakers1@users.noreply.github.com>
2021-12-06 05:37:56 -0600

    Add files via upload

---
+++ takumi/KAZ-N20/AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_KAZ-N20.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_KAZ-N20-user \
+    omni_KAZ-N20-userdebug \
+    omni_KAZ-N20-eng
